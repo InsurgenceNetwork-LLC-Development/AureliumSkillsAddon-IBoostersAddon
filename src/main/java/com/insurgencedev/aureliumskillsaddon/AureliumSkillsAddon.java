da1bb04f88ebd4d28c363788801d12c14289835d
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 01:51:18 -0500

    updated multi handling

--- src/main/java/com/insurgencedev/aureliumskillsaddon/AureliumSkillsAddon.java
+++ src/main/java/com/insurgencedev/aureliumskillsaddon/AureliumSkillsAddon.java
@@ -1,14 +1,17 @@
 package com.insurgencedev.aureliumskillsaddon;
 
 import com.insurgencedev.aureliumskillsaddon.listeners.AureliumSkillsEventListener;
+import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
-@IBoostersAddon(name = "AureliumSkillsAddon", version = "1.0.0", author = "InsurgenceDev", description = "AureliumSkills Support")
+@IBoostersAddon(name = "AureliumSkillsAddon", version = "1.0.1", author = "InsurgenceDev", description = "AureliumSkills Support")
 public class AureliumSkillsAddon extends InsurgenceBoostersAddon {
 
     @Override
     public void onAddonReloadablesStart() {
-        registerEvent(new AureliumSkillsEventListener());
+        if (Bukkit.getPluginManager().isPluginEnabled("AureliumSkills")) {
+            registerEvent(new AureliumSkillsEventListener());
+        }
     }
 }
